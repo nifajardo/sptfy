6b27dc617de21cb6657c06b9e37d08b788f87a22
nifajardo <73606587+nifajardo@users.noreply.github.com>
2020-10-29 01:46:05 +0800

    Update authorize.js

--- authorize.js
+++ authorize.js
@@ -5,9 +5,9 @@ $( document ).ready(function() {
   let client_id = '9ff915fc57e646e9af5e89bb9d6df6c7';
   // Use the following site to convert your regular url to the encoded version: 
   // https://www.url-encode-decode.com/
-  let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only';
+  let redirect_uri = 'https://nifajardo.github.io/sptfy';
   // *************** END *************************
-  const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+  const redirect = `https://accounts.spotify.com/authorize?client_id=9ff915fc57e646e9af5e89bb9d6df6c7&response_type=token&redirect_uri=https%3A%2F%2Fnifajardo.github.io%2Fsptfy`;
   console.log(`redirect url: ${redirect}`);
   window.location.replace(redirect);
 });
