7c3d388ad5facc06de1fd6256511eab709d8d723
nifajardo <73606587+nifajardo@users.noreply.github.com>
2020-10-30 00:41:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const inputElement = document.querySelector("inputValue");
   const value = inputValue.value;
   const url = 'https://api.spotify.com/v1/search?q=';
   const newUrl = url + value +'&type=track';
-     
+      $('#content').clear();
       $.ajax ({
       url: newUrl,
       type: 'GET',
