570923abafd4c6bada9327f542c2f3a462cd72ec
nifajardo <73606587+nifajardo@users.noreply.github.com>
2020-10-30 00:24:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const inputElement = document.querySelector("inputValue");
         let src_str = `https://open.spotify.com/embed/track/${id}`;
         console.log(`src_str ${src_str}`);
         let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
-        document.getElementById(elementID).innerHTML = "";
+        $('#content').empty();
         let parent_div = $('#content');
         parent_div.append(iframe);
       }
